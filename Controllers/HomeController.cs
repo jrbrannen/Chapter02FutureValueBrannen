6219e94705920f6beb82386609ac483a2078a786
jrbrannen <71732832+jrbrannen@users.noreply.github.com>
2021-01-28 16:32:47 -0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿// Jeremy Brannen
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Chapter02FutureValueBrannen.Models;
+
+namespace Chapter02FutureValueBrannen.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+
+            ViewBag.FV = 0;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(FutureValueModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.FV = model.CalculateFutureValue();
+            }
+            else
+            {
+                ViewBag.FV = 0;
+            }
+            return View(model);
+        }
+    }
+}
+
---
+++ Models/FutureValueModel.cs
@@ -0,0 +1,39 @@
+﻿// Jeremy Brannen
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Chapter02FutureValueBrannen.Models
+{
+    public class FutureValueModel
+    {
+        [Required(ErrorMessage = "Plese enter a monthly investment.")]
+        [Range(1,1000, ErrorMessage = "Monthly investment amount must be between 1 and 1000.")]
+        public decimal? MonthlyInvestment { get; set; }
+
+        [Required(ErrorMessage = "Please enter a yearly interest rate.")]
+        [Range(0.1, 10.0,  ErrorMessage = "Yearly interest rate much be between 0.1 and 10.0")]
+        public decimal? YearlyInterestRate { get; set; }
+
+        [Required(ErrorMessage = "Please enter a number of years.")]
+        [Range(1, 50, ErrorMessage = "Number of years must be between 1 and 50.")]
+
+        public int Years { get; set; }
+
+        public decimal CalculateFutureValue()
+        {
+            int months = Years * 12;
+            decimal monthlyInterestRate = YearlyInterestRate.Value / 12 / 100;
+            decimal futureValue = 0;
+            for( int i = 0; i < months; i++)
+            {
+                futureValue = (futureValue + MonthlyInvestment.Value) *
+                    (1 + monthlyInterestRate);
+            }
+            return futureValue;
+        }
+
+    }
+}
